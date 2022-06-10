a46bca1813e1cf1aede03abb7b5064596c639833
Don Heshanthaka <heshanthaka007@gmail.com>
2022-06-10 22:25:24 +0530

    Implementation of a python script to delete duplicate images

--- scripts/image_rename.py
+++ scripts/image_rename.py
@@ -1,8 +1,11 @@
 import os
 count = 1
-path = "copy paste the directory here"
+path = "D:\Projects\Python\Projects\Price Finder\web_scraper_for_image_collection\images\Wangon R stingray 2018"
+print(os.listdir(path))
 for dirname in os.listdir(path):
+    print(path + "\\" + dirname)
+    dirname = path + "\\" + dirname
     if os.path.isdir(dirname):
         for filename in (os.listdir(dirname)):
-            os.rename(dirname + "/" + filename, dirname + "/" + "0" + str(count) + ".jpg")
+            os.rename(dirname + "/" + filename, dirname + "/" + "00" + str(count) + ".jpg")
             count += 1
