a5f96270d3a6c8f18e2605ab678e252f1031eb97
Don Heshanthaka <heshanthaka007@gmail.com>
2022-06-11 01:30:44 +0530

    Bug fix on download_image function, indefinite waiting on response in requests.get() method, fixed by adding a timeout

--- scripts/image_rename.py
+++ scripts/image_rename.py
@@ -1,11 +1,38 @@
+from difPy import dif
 import os
-count = 1
-path = "D:\Projects\Python\Projects\Price Finder\web_scraper_for_image_collection\images\Wangon R stingray 2018"
-print(os.listdir(path))
-for dirname in os.listdir(path):
-    print(path + "\\" + dirname)
-    dirname = path + "\\" + dirname
-    if os.path.isdir(dirname):
+import shutil
+from random import randint
+
+PATH = "D:\Projects\Python\Projects\Price Finder\web_scraper_for_image_collection\images\hero collection"
+TEMP_COLLECTION_PATH = PATH + "\collection"
+os.mkdir(TEMP_COLLECTION_PATH)
+DST_PATH = "D:\Projects\Python\Projects\Price Finder\web_scraper_for_image_collection\images\\Hero Dash 2016"
+#os.mkdir(DST_PATH)
+
+# print(os.listdir(PATH))
+
+for dirname in os.listdir(PATH):
+    #print(PATH + "\\" + dirname)
+    dirname = PATH + "\\" + dirname
+    if os.path.isdir(dirname) and dirname != TEMP_COLLECTION_PATH:
         for filename in (os.listdir(dirname)):
-            os.rename(dirname + "/" + filename, dirname + "/" + "00" + str(count) + ".jpg")
-            count += 1
+            #print(DST_PATH + "/" + "0" + str(COUNT) + ".jpg")
+            #os.rename(dirname + "/" + filename, DST_PATH + "/" + "0" + str(COUNT) + ".jpg")
+            src = dirname + "\\" + filename
+            #dst =  DST_PATH + "/" + "0" + str(COUNT) + ".jpg"
+            dst = TEMP_COLLECTION_PATH + "\\" + str(randint(0, 999)) + filename
+            shutil.copy(src, dst)
+            print(dirname + "\\" + filename)
+
+
+# search = dif(TEMP_COLLECTION_PATH, similarity="low",
+#              show_progress=True, delete=True, silent_del=True)
+
+
+# COUNT = 1
+# for filename in (os.listdir(TEMP_COLLECTION_PATH)):
+#     print(DST_PATH + "\\" + "0" + str(COUNT) + ".jpg")
+#     src = TEMP_COLLECTION_PATH + "/" + filename
+#     dst =  DST_PATH + "\\" + "0" + str(COUNT) + ".jpg"
+#     shutil.copy(src, dst)
+#     COUNT += 1
--- web_scraper_for_image_collection/web_scraping_google_images.py
+++ web_scraper_for_image_collection/web_scraping_google_images.py
@@ -6,14 +6,14 @@ import time
 
 
 #creating a directory to save images
-folder_name = 'images\\alto 05'
+folder_name = 'images\\aqua 02'
 if not os.path.isdir(folder_name):
     os.makedirs(folder_name)
 
 def download_image(url, folder_name, num):
-
     # write image to file
-    reponse = requests.get(url)
+    
+    reponse = requests.get(url, timeout=2)
     if reponse.status_code==200:
         with open(os.path.join(folder_name, str(num)+".jpg"), 'wb') as file:
             file.write(reponse.content)
@@ -22,7 +22,7 @@ def download_image(url, folder_name, num):
 chromeDriverPath = "D:\Projects\Python\Web_Scraping_Selenium\chromedriver.exe"
 driver = webdriver.Chrome(chromeDriverPath)
 
-search_URL = "https://www.google.lk/search?q=maruti+suzuki+alto+800+2015+sri+lanka&tbm=isch&ved=2ahUKEwi1sMzsh6X4AhWvBbcAHRNRCOgQ2-cCegQIABAA&oq=maruti+suzuki+alto+800+2015+sri+lanka&gs_lcp=CgNpbWcQA1DzDVixGGCGG2gAcAB4AIABhAOIAbULkgEHMC45LjAuMZgBAKABAaoBC2d3cy13aXotaW1nwAEB&sclient=img&ei=GV2kYvW_Ea-L3LUPk6KhwA4&bih=937&biw=1920"
+search_URL = "https://www.google.lk/search?q=prius+c+2014&hl=en&tbm=isch&source=hp&biw=1920&bih=937&ei=4OSkYo_XLMXC3LUPqq-c6AI&iflsig=AJiK0e8AAAAAYqTy8PdTMeIfQ53xaKBJHonLkO1sfeAN&ved=0ahUKEwiPx5iriab4AhVFIbcAHaoXBy0Q4dUDCAY&uact=5&oq=prius+c+2014&gs_lcp=CgNpbWcQAzIFCAAQgAQyBQgAEIAEMgUIABCABDIGCAAQHhAFMgYIABAeEAgyBggAEB4QCDIGCAAQHhAIMgYIABAeEAgyBggAEB4QCDIGCAAQHhAIOggIABCABBCxAzoICAAQsQMQgwE6CwgAEIAEELEDEIMBUABY8RhgxRpoAHAAeACAAckBiAHvDpIBBjAuMTEuMZgBAKABAaoBC2d3cy13aXotaW1n&sclient=img"
 
 driver.get(search_URL)
 
@@ -75,7 +75,7 @@ for i in range(1, len_containers+1):
             imageElement = driver.find_element_by_xpath("""//*[@id="Sva75c"]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div[3]/div/a/img""")
         except:
             continue
-        
+
         imageURL= imageElement.get_attribute('src')
 
 
@@ -108,3 +108,10 @@ driver.quit()
 
 # Alto 2018
 # https://www.google.lk/search?q=maruti+suzuki+alto+800+2015&tbm=isch&ved=2ahUKEwi5vZfOr6P4AhWr1HMBHaV5DdEQ2-cCegQIABAA&oq=maruti+suzuki+alto+800+2015&gs_lcp=CgNpbWcQAzIFCAAQgAQyBQgAEIAEMgYIABAeEAgyBggAEB4QCFDVB1iCDGDNDWgAcAB4AIABlAGIAf0CkgEDMC4zmAEAoAEBqgELZ3dzLXdpei1pbWfAAQE&sclient=img&ei=W3qjYvmjG6upz7sPpfO1iA0&bih=937&biw=1920
+
+# Hero dash
+# https://www.google.lk/search?q=hero+dash+2016&tbm=isch&ved=2ahUKEwigmuTBjqX4AhUcjNgFHeBnDLsQ2-cCegQIABAA&oq=hero+dash+2016&gs_lcp=CgNpbWcQAzIFCAAQgAQyBQgAEIAEMgQIABAYMgQIABAYMgQIABAYOgQIABBDOgsIABCABBCxAxCDAToICAAQgAQQsQM6BwgAELEDEEM6BggAEB4QBToECAAQHlDCCljDHmCRJGgAcAB4AIABugOIAdITkgEIMC4xNC40LTGYAQCgAQGqAQtnd3Mtd2l6LWltZ8ABAQ&sclient=img&ei=FmSkYqC3IZyY4t4P4M-x2As&bih=937&biw=1920
+
+
+# Aqua
+#https://www.google.lk/search?q=toyota+aqua+2014&tbm=isch&ved=2ahUKEwiRvMaNzKX4AhWlKbcAHbieC8IQ2-cCegQIABAA&oq=toyota+aqua+2014&gs_lcp=CgNpbWcQARgAMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBggAEB4QCDIGCAAQHhAIOgQIABBDOggIABCABBCxAzoICAAQsQMQgwE6BwgAELEDEENQtBBYl1Vg3lZoA3AAeACAAdACiAGZF5IBCDAuMTYuMS4xmAEAoAEBqgELZ3dzLXdpei1pbWfAAQE&sclient=img&ei=rKSkYpE2pdPctQ-4va6QDA&bih=937&biw=1920
