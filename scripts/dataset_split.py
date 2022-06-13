3394cfb755f18fd0fc44a61f6c2dd7856b4b841b
Don Heshanthaka <heshanthaka007@gmail.com>
2022-06-13 19:55:08 +0530

    Created a script to split the data between test and training

---
+++ scripts/dataset_split.py
@@ -0,0 +1,37 @@
+import os
+
+import numpy as np
+
+import shutil
+
+rootdir = "D:\Projects\Python\Projects\Price Finder\web_scraper_for_image_collection\images\\final" # path of the original folder
+
+classes = ["Alto 2015", "Hero Dash 2016", "Toyota Aqua 2014", "Wagon R Stingray 2018"]
+
+for i in classes:
+
+    os.makedirs(rootdir + '/train/' + i)
+
+    os.makedirs(rootdir + '/test/' + i)
+
+    source = rootdir + '/' + i
+
+    allFileNames = os.listdir(source)
+
+    np.random.shuffle(allFileNames)
+
+    test_ratio = 0.20
+
+    train_FileNames, test_FileNames = np.split(np.array(allFileNames),
+                                            [int(len(allFileNames) * (1 - test_ratio))])
+
+    train_FileNames = [source+'/' + name for name in train_FileNames.tolist()]
+    test_FileNames = [source+'/' + name for name in test_FileNames.tolist()]
+
+    print(train_FileNames)
+
+    for name in train_FileNames:
+        shutil.copy(name, rootdir + '/train/' + i)
+
+    for name in test_FileNames:
+        shutil.copy(name, rootdir + '/test/' + i)
