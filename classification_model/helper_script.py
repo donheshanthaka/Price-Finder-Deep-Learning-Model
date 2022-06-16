58a81ad51821b26f126ec7abf7efc1140223189d
Don Heshanthaka <heshanthaka007@gmail.com>
2022-06-16 18:14:05 +0530

    Implementation of functions to save and load TensorFlow models

--- classification_model/helper_script.py
+++ classification_model/helper_script.py
@@ -124,3 +124,21 @@ def compare_history(original_history, new_history, initial_epochs=5):
     plt.plot([initial_epochs-1, initial_epochs-1], plt.ylim(), label="Start Fine Tuning")
     plt.legend(loc="upper right")
     plt.title("Training and Validation Loss")
+
+
+def walk_through_dir(dir_path):
+    """
+    Walks through dir_path returning its contents.
+
+    Args:
+        dir_path (str): target directory
+    
+    Returns:
+        A print out of:
+        number of subdiretories in dir_path
+        number of images (files) in each subdirectory
+        name of each subdirectory
+    """
+    for dirpath, dirnames, filenames in os.walk(dir_path):
+        print(f"There are {len(dirnames)} directories and {len(filenames)} images in '{dirpath}'.")
+    
