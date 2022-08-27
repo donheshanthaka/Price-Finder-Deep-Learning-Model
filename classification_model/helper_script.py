892d3a67e6b8f976c8c5acdc770db84008cd46e8
Don Heshanthaka <heshanthaka007@gmail.com>
2022-08-27 21:10:52 +0530

    Added load_and_prep_image function to the helper_script

--- classification_model/helper_script.py
+++ classification_model/helper_script.py
@@ -168,3 +168,34 @@ def load_model(model_name):
     """
     location = "saved_models" + "\\" + model_name
     return tf.keras.models.load_model(location)
+
+
+def load_and_prep_image(filename, img_shape=224, scale=True):
+  """
+  Reads in an image from filename, turns it into a tensor and reshapes into
+  specified shape (img_shape, img_shape, color_channels=3).
+
+  Args:
+    filename (str): path to target image
+    image_shape (int): height/width dimension of target image size
+    scale (bool): scale pixel values from 0-255 to 0-1 or not
+  
+  Returns:
+    Image tensor of shape (img_shape, img_shape, 3)
+  """
+  # Read in the image
+  img = tf.io.read_file(filename)
+
+  # Decode image into tensor
+  img = tf.io.decode_image(img, channels=3)
+
+  # Resize the image
+  img = tf.image.resize(img, [img_shape, img_shape],preserve_aspect_ratio=False)
+
+  # Scale? Yes/no
+  if scale:
+    # rescale the image (get all values between 0 and 1)
+    return img/255.
+  else:
+    return img # don't need to rescale images for EfficientNet models in TensorFlow
+
