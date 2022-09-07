1563103a3a7a58506d6e5639c7efbd34d156af9f
Don Heshanthaka <heshanthaka007@gmail.com>
2022-09-07 09:24:10 +0530

    Deleted scripts folder

--- scripts/duplicate_image_finder.py
+++
@@ -1,2 +0,0 @@
-from difPy import dif
-search = dif("D:\Projects\Python\Projects\Price Finder\web_scraper_for_image_collection\images\Wangon R stingray 2018", similarity="low", show_progress=True, delete=True, silent_del=True)
